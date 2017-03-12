1da7d7b97278885e0fb8be2cec67d24538fa61e5
Christian McHugh <McHughC@DNB.com>
2017-03-12 09:46:15 +0000

    Add test for keyword arguments with multiple =

--- src/test/java/com/waytta/BuildsTest.java
+++ src/test/java/com/waytta/BuildsTest.java
@@ -32,4 +32,28 @@ public class BuildsTest {
 
         Assert.assertTrue(expectedResult.equals(saltFunc));
     }
+
+	@Test
+	public void testMultipleEquals() {
+		String myarguments = "thing thing2 keyword=value=thing \"arg with space\"";
+
+		JSONObject saltFunc = new JSONObject();
+		saltFunc.put("client", "local");
+		saltFunc.put("tgt", "testTarget");
+		saltFunc.put("expr_form", "glob");
+		saltFunc.put("fun", "cmd.run");
+
+		Builds.addArgumentsToSaltFunction(myarguments, saltFunc);
+
+		JSONObject expectedResult = JSONObject.fromObject("{\"client\":\"local\","
+				+ "\"tgt\":\"testTarget\","
+				+ "\"expr_form\":\"glob\","
+				+ "\"fun\":\"cmd.run\","
+				+ "\"arg\":[\"thing\", \"thing2\", \"arg with space\"],"
+				+ "\"kwarg\":{\"keyword\":\"value=thing\"},"
+				+ "}");
+
+		System.out.println(saltFunc.toString());
+        Assert.assertTrue(expectedResult.equals(saltFunc));
+	}
 }
